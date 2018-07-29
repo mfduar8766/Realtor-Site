c5e4ea87c64e2138621295ddf0a6179cd2174fcd
Matheus Duarte <mfduar8766@gmail.com>
2018-07-29 09:24:28 -0400

    Testing out async function and modified some HTML/CSS.

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
+window.onload = function() {
+    var http = new XMLHttpRequest();
+
+    //Listen to request state changes
+    http.onreadystatechange = function() {
+        if(http.readyState === 4 && http.status === 200) {
+            console.log(JSON.parse(http.response));
+        }
+    } 
+    
+    //Set up request Request type, URL, if its async:true or sync:false
+    http.open("GET", "homes.json", true);
+
+     //Send(): To grab the data
+    http.send();
+};
+
 var menu = document.querySelector('.menu');
 var list = document.querySelector('.nav-list')
 
 var openClose = function() {
     list.classList.toggle('show');
-}
+};
 
 menu.addEventListener('click', openClose);
 
