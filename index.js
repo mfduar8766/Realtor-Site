4793e91dc1a21e209b458f8956315a8a2d70725f
Matheus Duarte <mfduar8766@gmail.com>
2018-08-04 13:01:57 -0400

    Changed some CSS and removed AJAX call and replaced with Fetch.

--- index.js
+++ index.js
@@ -1,29 +1,49 @@
-var home1Img = document.querySelector('.home1-img');
-var home2Img = document.querySelector('.home2-img');
-var home3Img = document.querySelector('.home3-img');
-var home4Img = document.querySelector('.home4-img');
-var home5Img = document.querySelector('.home5-img');
-var menu = document.querySelector('.menu');
-var list = document.querySelector('.nav-list')
+var menu = document.querySelector(".menu");
+var list = document.querySelector(".nav-list");
 
-window.onload = function() {
-    var xml = new XMLHttpRequest();
-    xml.open('GET', 'homes.json', true);
-    xml.onload = function() {
-        if(this.status === 200) {
-            var homes = JSON.parse(this.response);
-            home1Img.setAttribute('src', homes[0].image);
-            home2Img.setAttribute('src', homes[1].image);
-            home3Img.setAttribute('src', homes[2].image);
-            home4Img.setAttribute('src', homes[3].image);
-            home5Img.setAttribute('src', homes[4].image);
-        }    
-    }
-    xml.send();
+var home1 = function(data) {
+    var home = document.querySelector(".home1-img");
+    home.setAttribute('src', data);
+    return home;
 }
 
+var home2 = function(data) {
+    var home = document.querySelector(".home2-img");
+    home.setAttribute('src', data);
+    return home;
+}
+
+var home3 = function(data) {
+    var home =document.querySelector(".home3-img");
+    home.setAttribute('src', data);
+    return home;
+}
+
+var home4 = function(data) {
+    var home = document.querySelector(".home4-img");
+    home.setAttribute('src', data);
+    return home;
+}
+
+var home5 = function(data) {
+    var home = document.querySelector(".home5-img");
+    home.setAttribute('src', data);
+    return home;
+}
+
+fetch('homes.json')
+.then((res) => res.json())
+.then((data) => {
+    home1(data[0].image);
+    home2(data[1].image);
+    home3(data[2].image);
+    home4(data[3].image);
+    home5(data[4].image);
+}).catch((error) => console.log(error))
+
+
 var openClose = function() {
-    list.classList.toggle('show');
+  list.classList.toggle("show");
 };
 
-menu.addEventListener('click', openClose);
+menu.addEventListener("click", openClose);
