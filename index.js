75fc6f8ab824f0652fb2cf327f5c423c58ae1c7e
Matheus Duarte <mfduar8766@gmail.com>
2018-09-07 09:55:20 -0400

    Changed the js file.

--- index.js
+++ index.js
@@ -2,45 +2,45 @@ var menu = document.querySelector(".menu");
 var list = document.querySelector(".main-nav");
 
 var home1 = function(data) {
-    var home = document.querySelector(".home1-img");
-    home.setAttribute('src', data);
-    return home;
-}
+  var home = document.querySelector(".home1-img");
+  home.setAttribute("src", data);
+  return home;
+};
 
 var home2 = function(data) {
-    var home = document.querySelector(".home2-img");
-    home.setAttribute('src', data);
-    return home;
-}
+  var home = document.querySelector(".home2-img");
+  home.setAttribute("src", data);
+  return home;
+};
 
 var home3 = function(data) {
-    var home =document.querySelector(".home3-img");
-    home.setAttribute('src', data);
-    return home;
-}
+  var home = document.querySelector(".home3-img");
+  home.setAttribute("src", data);
+  return home;
+};
 
 var home4 = function(data) {
-    var home = document.querySelector(".home4-img");
-    home.setAttribute('src', data);
-    return home;
-}
+  var home = document.querySelector(".home4-img");
+  home.setAttribute("src", data);
+  return home;
+};
 
 var home5 = function(data) {
-    var home = document.querySelector(".home5-img");
-    home.setAttribute('src', data);
-    return home;
-}
+  var home = document.querySelector(".home5-img");
+  home.setAttribute("src", data);
+  return home;
+};
 
-fetch('homes.json')
-.then((res) => res.json())
-.then((data) => {
+fetch("homes.json")
+  .then(res => res.json())
+  .then(data => {
     home1(data[0].image);
     home2(data[1].image);
     home3(data[2].image);
     home4(data[3].image);
     home5(data[4].image);
-}).catch((error) => console.log(error))
-
+  })
+  .catch(error => console.log(error));
 
 var openClose = function() {
   list.classList.toggle("show");
